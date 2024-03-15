79374d7ccc5c26ece7dcaf017b6d003ef5590b84
Ilya M. <wuvuxd@proton.me>
2024-03-15 13:24:42 +0700

    fix heading

--- .bash/generate-readme.sh
+++ .bash/generate-readme.sh
@@ -6,7 +6,7 @@ tempfile=$(mktemp)
 find . -maxdepth 1 -type d \( ! -name ".*" \) | while read -r item; do
     item=$(basename "$item")
     if [[ "$item" != "." ]]; then
-        echo "## $item" >> "$tempfile"
+        echo "### $item" >> "$tempfile"
         find "$item" -maxdepth 1 -type f -name "*.md" | sort -t. -k1,1n | sort -V | while read -r file; do
             file=$(basename "$file" .md)
             folder=$(basename "$item" | sed 's/ /%20/g')
