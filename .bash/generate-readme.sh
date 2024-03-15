a02348a0d070fae01888d845e1ba3fade61c901a
Ilya M. <wuvuxd@proton.me>
2024-03-15 13:22:45 +0700

    update solutions and yaml

--- .bash/generate-readme.sh
+++ .bash/generate-readme.sh
@@ -6,10 +6,10 @@ tempfile=$(mktemp)
 find . -maxdepth 1 -type d \( ! -name ".*" \) | while read -r item; do
     item=$(basename "$item")
     if [[ "$item" != "." ]]; then
-        echo "\n## $item\n" >> "$tempfile"
+        echo "## $item" >> "$tempfile"
         find "$item" -maxdepth 1 -type f -name "*.md" | sort -t. -k1,1n | sort -V | while read -r file; do
             file=$(basename "$file" .md)
-            folder=$(basename "$item")
+            folder=$(basename "$item" | sed 's/ /%20/g')
             file_link=$(echo "$file" | sed 's/ /%20/g')
             echo "- [$file](https://github.com/koshacha/leetcode/blob/main/$folder/$file_link.md)" >> "$tempfile"
         done
--- .bash/readme-template.md
+++ .bash/readme-template.md
@@ -1,4 +1,10 @@
-# leetcode
+# My solutions for algorithmic problems from LeetCode.
 
-algorithmic problem solving practice
+![GitHub last commit (branch)](https://img.shields.io/github/last-commit/koshacha/leetcode/main?style=for-the-badge&logo=leetcode)
+
+- 👨‍💻 [My Account](https://leetcode.com/koshacha/)
+- 🚀 Languages used: mostly TypeScript and JavaScript.
+
+## Solutions
+
 %LIST%
