d4c488c8803383f8b9c7499510ff80e1073c2d51
Ilya M. <wuvuxd@proton.me>
2024-03-15 12:32:10 +0700

    yaml test

---
+++ .github/workflows/generate-readme.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+template=$(<readme-template.md)
+
+tempfile=$(mktemp)
+find . -maxdepth 1 -type d \( ! -name ".*" \) | while read -r item; do
+    item=$(basename "$item")
+    if [[ "$item" != "." ]]; then
+        echo "- $item" >> "$tempfile"
+        find "$item" -maxdepth 1 -type f -name "*.md" | sort -t. -k1,1n | sort -V | while read -r file; do
+            file=$(basename "$file" .md)
+            folder=$(basename "$item")
+            file_link=$(echo "$file" | sed 's/ /%20/g')
+            echo "  - [$file](https://github.com/koshacha/leetcode/blob/main/$folder/$file_link.md)" >> "$tempfile"
+        done
+    fi
+done
+
+list=$(<"$tempfile")
+output="${template/\%LIST%/$list}"
+echo $output
+
+rm "$tempfile"
---
+++ .github/workflows/readme-template.md
@@ -0,0 +1,5 @@
+# leetcode
+
+algorithmic problem solving practice
+
+%LIST%
