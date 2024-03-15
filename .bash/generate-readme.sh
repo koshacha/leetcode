32ac357d94734a93317b79868b1675eee08f0cef
Ilya M. <wuvuxd@proton.me>
2024-03-15 19:31:08 +0700

    edit template

--- .bash/generate-readme.sh
+++ .bash/generate-readme.sh
@@ -7,10 +7,10 @@ tempfile_sum=$(mktemp)
 find . -maxdepth 1 -type d \( ! -name ".*" \) | while read -r item; do
     item=$(basename "$item")
     if [[ "$item" != "." ]]; then
-        echo "### $item" >> "$tempfile"
+        echo "#### $item" >> "$tempfile"
         url=$(echo "$item" | tr '[:upper:]' '[:lower:]')
         url=$(echo "$url" | sed 's/ /-/g')
-        echo "[1. $item](#$url)" >> "$tempfile_sum"
+        echo "1. [$item](#$url)" >> "$tempfile_sum"
         find "$item" -maxdepth 1 -type f -name "*.md" | sort -t. -k1,1n | sort -V | while read -r file; do
             file=$(basename "$file" .md)
             folder=$(basename "$item" | sed 's/ /%20/g')
--- .bash/readme-template.md
+++ .bash/readme-template.md
@@ -1,14 +1,14 @@
-# My solutions for algorithmic problems from LeetCode.
+## My solutions for algorithmic problems from LeetCode.
 
 ![GitHub last commit (branch)](https://img.shields.io/github/last-commit/koshacha/leetcode/main?style=for-the-badge&logo=leetcode)
 
 - 👨‍💻 [My Account](https://leetcode.com/koshacha/)
 - 🚀 Languages used: mostly TypeScript and JavaScript.
 
-## List of Topics
+### List of Topics
 
 %SUMMARY%
 
-## Solutions
+### Solutions
 
 %LIST%
